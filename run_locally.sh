c62b1df53dcaf0719512c09139d1974c2082e89f
Kunal Marwaha <marwahaha@berkeley.edu>
2019-05-23 18:21:09 -0700

    Save output of run_locally script

--- run_locally.sh
+++ run_locally.sh
@@ -3,6 +3,10 @@
 ## Should be run after Turbine.msh is created
 ## Should be run only in this directory
 
+## Store outputs in new folder in localruns
+FOLDER=localruns/run.`ls localruns/ | wc -l`
+mkdir -p $FOLDER
+echo "Start time:" `date` >> $FOLDER/timings.txt
 
 ## Delete older intermediate files (except Turbine.msh)
 rm -rf pimplefoam.log
@@ -10,6 +14,7 @@ rm -rf postProcessing
 rm -rf processor*
 rm -rf *.pos
 rm -rf constant/polyMesh
+echo "Finished removing old files:" `date` >> $FOLDER/timings.txt
 
 ## Get ready for OpenFOAM computation
 gmshToFoam ./Turbine.msh
@@ -18,6 +23,16 @@ renumberMesh -overwrite
 rm -rf 0
 cp -rf 0.orig 0
 decomposePar
+echo "Now ready for OpenFOAM computation:" `date` >> $FOLDER/timings.txt
 
 ## Run OpenFOAM computation
 mpirun -np 4 pimpleFoam -parallel | tee pimplefoam.log
+echo "Done with OpenFOAM computation:" `date` >> $FOLDER/timings.txt
+
+## Copy files to output folder
+cp postProcessing/forces/0/coefficient.dat $FOLDER/
+cp pimplefoam.log $FOLDER/
+cp system/controlDict $FOLDER/
+cp system/fvSchemes $FOLDER/
+cp system/fvSolution $FOLDER/
+echo "Done copying files:" `date` >> $FOLDER/timings.txt
